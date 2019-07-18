218a07e2e1eab3c9aacb7b5d3a6c338c9194cfaf
jacobhjustice <jacobhj1997@gmail.com>
2019-07-17 22:29:37 -0500

    front end changes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ class Timer extends React.Component {
         this.state = {
             time: new Date(),
         }
+
+        this.months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
         // Update the clock every second
         setInterval(() => this.setState({
             time: new Date(),
@@ -16,8 +20,11 @@ class Timer extends React.Component {
 
     render() {
         return (
-           <h2>{this.parseTime()}</h2>
-        )
+            <div id="clock">
+                <div class="header">{this.parseTime()}</div>
+                <div class="subtitle">{this.months[this.state.time.getMonth()]} {this.state.time.getDate()}, {this.state.time.getFullYear()}</div>
+            </div>
+        );
     }
 
     parseTime() {
@@ -25,19 +32,51 @@ class Timer extends React.Component {
         var isPM = time.getHours() >= 12
         var hours =  (time.getHours() + 11) % 12 + 1
         var minutes = time.getMinutes()
+
         return  (hours < 10 ? "0" : "") + hours  
         + ":" + (minutes < 10 ? "0" : "") + minutes 
         + " " + (isPM ? "PM" : "AM")
     }
+}
 
-    get
+class Verse extends React.Component {
+    constructor() {
+        super()
+
+        this.getVerse()
+        this.state = {
+            quote: "Humble yourselves, therefore, under the mighty hand of God, so that at the proper time he may exult you, casting all your anxieties on him because he cares for you",
+            reference: "1 Peter 5: 6-7"
+        }
+    }
+
+    getVerse() {
+        // Server request to scrape here
+        // Update State
+    }
+
+
+    render() {
+        return (
+            <div id="verse">
+                <div class="wrapper">
+                    <div class="quote">"{this.state.quote}"</div>
+                    <div class="reference">-  {this.state.reference}</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 class Frame extends React.Component {
     render() {
-      return (
-        <Timer/>
-      );
+        return (
+            <div id="frame">
+                <Timer />
+                <Verse />
+            </div>
+        
+        ); 
     }
   }
 
